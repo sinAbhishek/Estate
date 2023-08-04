f11f3c828c3ead4b6bb29c741c2f3ef30918be21
sinAbhishek <singhabhshek979@gmail.com>
2023-08-04 22:40:46 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,74 @@
+const data=[{
+    id:1,
+    name:"Ct Duplex",
+    place:"Oregon",
+    type:"House",
+    address:"600-605 S 41st Ct, Springfield, OR 97478",
+    desc:["2 Beds","3 Bathroom","5*7 msq"],
+    image:"https://images1.loopnet.com/i2/rle890NwXdCuuiyLUi2WD5b1TzihbvNLXQFGImyANpo/116/600-605-S-41st-Ct-Springfield-OR-607-S-41st-Ct-29-RMLS-1-LargeHighDefinition.jpg"
+
+},{
+    id:2,
+    name:"Hillsboro",
+    place:"Oregon",
+    type:"House",
+    address:"16350 NE Hillsboro Hwy, Newberg, OR 97132",
+    desc:["2 Beds","3 Bathroom","5*7 msq"],
+    image:"https://images1.loopnet.com/i2/xp0U9XtQWAUPEa-FKFwHIY6bx_-Uo5B12BpQt4Ft6MY/116/16350-NE-Hillsboro-Hwy-Newberg-OR-1-web-or-mls-16350-ne-hillsboro-hwy-1-LargeHighDefinition.jpg"
+},{
+    id:1,
+    name:"Combs",
+    place:"Oregon",
+    type:"Office",
+    address:"200 SE Combs Flat Rd, Prineville, OR 97754",
+    desc:["2 Beds","3 Bathroom","5*7 msq"],
+    image:
+},{
+    id:1,
+    name:"Ct Duplex",
+    place:"Oregon",
+    type:"House",
+    address:"600-605 S 41st Ct, Springfield, OR 97478",
+    desc:["2 Beds","3 Bathroom","5*7 msq"],
+    image:
+},{
+    id:1,
+    name:"Ct Duplex",
+    place:"Oregon",
+    type:"House",
+    address:"600-605 S 41st Ct, Springfield, OR 97478",
+    desc:["2 Beds","3 Bathroom","5*7 msq"],
+    image:
+},{
+    id:1,
+    name:"Ct Duplex",
+    place:"Oregon",
+    type:"House",
+    address:"600-605 S 41st Ct, Springfield, OR 97478",
+    desc:["2 Beds","3 Bathroom","5*7 msq"],
+    image:
+},{
+    id:1,
+    name:"Ct Duplex",
+    address:"600-605 S 41st Ct, Springfield, OR 97478",
+    desc:["2 Beds","3 Bathroom","5*7 msq"],
+    image:
+},{
+    id:1,
+    name:"Ct Duplex",
+    address:"600-605 S 41st Ct, Springfield, OR 97478",
+    desc:["2 Beds","3 Bathroom","5*7 msq"],
+    image:
+},{
+    id:1,
+    name:"Ct Duplex",
+    address:"600-605 S 41st Ct, Springfield, OR 97478",
+    desc:["2 Beds","3 Bathroom","5*7 msq"],
+    image:
+},{
+    id:1,
+    name:"Ct Duplex",
+    address:"600-605 S 41st Ct, Springfield, OR 97478",
+    desc:["2 Beds","3 Bathroom","5*7 msq"],
+    image:
+},]
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +15,34 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// import { addDays, differenceInCalendarDays, startOfDay } from "date-fns";
+// import Calendar from "react-calendar";
+// import "react-calendar/dist/Calendar.css";
+
+// import "./styles.css";
+// const date1=new Date("2023-08-04")
+
+// const tomorrow = addDays(now, 4);
+// const in3Days = addDays(now, 3);
+// const in5Days = addDays(now, 9);
+
+// const highlightedDates = [date1];
+
+// function isSameDay(a, b) {
+
+//   return differenceInCalendarDays(a, b) === 0;
+// }
+
+// export default function App() {
+//   function tileClassName({ date, view }) {
+
+//     if (
+//       view === "month" &&
+//       highlightedDates.find((c) => isSameDay(c, date))
+//     ) {
+//       return "highlight";
+//     }
+//   }
+
+//   return <Calendar  tileClassName={tileClassName} />;
+// }
